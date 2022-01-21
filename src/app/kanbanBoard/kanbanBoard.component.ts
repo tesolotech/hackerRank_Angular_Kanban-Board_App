f9ce9b5c1f73b2cb8e352ebf4da3ee9782efc007
Git Server Admin <admin@git-server>
2022-01-21 16:52:58 +0000

    Add initial repository

---
+++ src/app/kanbanBoard/kanbanBoard.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'kanban-board',
+  templateUrl: './kanbanBoard.component.html',
+  styleUrls: ['./kanbanBoard.component.scss']
+})
+export class KanbanBoard implements OnInit {
+  tasks: Task[];
+  stagesNames: string[];
+  stagesTasks: any[]; //Only used for rendering purpose
+
+  ngOnInit() {
+    // Each task is uniquely identified by its name. 
+    // Therefore, when you perform any operation on tasks, make sure you pick tasks by names (primary key) instead of any kind of index or any other attribute.
+    this.tasks = [
+      { name: '0', stage: 0 },
+      { name: '1', stage: 0 },
+    ];
+    this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
+    this.configureTasksForRendering();
+  }
+  
+  // this function has to be called whenever tasks array is changed to construct stagesTasks for rendering purpose
+  configureTasksForRendering = () => {
+    this.stagesTasks = [];
+    for (let i = 0; i < this.stagesNames.length; ++i) {
+      this.stagesTasks.push([]);
+    }
+    for (let task of this.tasks) {
+      const stageId = task.stage;
+      this.stagesTasks[stageId].push(task);
+    }
+  }
+
+  generateTestId = (name) => {
+    return name.split(' ').join('-');
+  }
+}
+
+interface Task {
+  name: string;
+  stage: number;
+}
