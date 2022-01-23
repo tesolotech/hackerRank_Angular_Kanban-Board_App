1df89540e3f8c77c5014b34bbd6832afbf2f6371
vikash <vikash.kumar@appzlogic.com>
2022-01-23 20:28:55 +0530

    Final Commit

--- src/app/kanbanBoard/kanbanBoard.component.ts
+++ src/app/kanbanBoard/kanbanBoard.component.ts
@@ -9,18 +9,18 @@ export class KanbanBoard implements OnInit {
   tasks: Task[];
   stagesNames: string[];
   stagesTasks: any[]; //Only used for rendering purpose
-
+  taskName: string;
   ngOnInit() {
-    // Each task is uniquely identified by its name. 
+    // Each task is uniquely identified by its name.
     // Therefore, when you perform any operation on tasks, make sure you pick tasks by names (primary key) instead of any kind of index or any other attribute.
     this.tasks = [
-      { name: '0', stage: 0 },
-      { name: '1', stage: 0 },
+      { name: 'Eat mango', stage: 0 },
+      { name: 'Have to read', stage: 1 },
     ];
     this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
     this.configureTasksForRendering();
   }
-  
+
   // this function has to be called whenever tasks array is changed to construct stagesTasks for rendering purpose
   configureTasksForRendering = () => {
     this.stagesTasks = [];
@@ -36,6 +36,34 @@ export class KanbanBoard implements OnInit {
   generateTestId = (name) => {
     return name.split(' ').join('-');
   }
+
+  createTask(taskName: any) {
+    if(taskName.trim().length) {
+      this.stagesTasks[0].push({ name: taskName, stage: 0 })
+      this.taskName = '';
+    }
+
+  }
+
+  task_back(superIndex, index) {
+    const task = JSON.parse(JSON.stringify(this.stagesTasks[superIndex][index]));
+    task['stage'] = task['stage'] - 1;
+    this.stagesTasks[superIndex].splice(index, 1);
+    this.stagesTasks[superIndex - 1].push(task);
+  }
+
+  task_forward(superIndex, index) {
+    const task = JSON.parse(JSON.stringify(this.stagesTasks[superIndex][index]));
+    task['stage'] = task['stage'] + 1;
+    this.stagesTasks[superIndex].splice(index, 1);
+    this.stagesTasks[superIndex + 1].push(task);
+  }
+
+  delteTask(superIndex, index) {
+    this.stagesTasks[superIndex].splice(index, 1);
+  }
+
+
 }
 
 interface Task {
